0b78c4fc05abdc0abf42cde089c2ff693053ed8b
seanmx96 <seanmx96@users.noreply.github.com>
2020-04-14 15:07:01 -0500

    Imported useState hooks in PostsPage.js and App.js. Rendered Search and Posts components in App.js.

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ PostsContainer components and the CommentSection Components.
 Not all files need code added. 
 Look at each file to see where you need to pass props or add code 
 */
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 // import the PostsPage and SearchBar and add them to the App
+import Posts from "./components/PostsContainer/PostsPage.js";
+import Search from "./components/SearchBar/SearchBarContainer.js";
 
 
 const App = () => {
@@ -14,6 +16,8 @@ const App = () => {
   return (
     <div className="App">
       {/* Add imported components here to render them */}
+      <Search />
+      <Posts />
     </div>
   );
 };
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,12 +1,14 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import React, { useState } from "react";
 import Post from "./Post";
 import "./Posts.css";
 // import data 
 
 const PostsPage = () => {
   // set up state for your data
+  const [posts, setPosts] = useState('')
+
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
