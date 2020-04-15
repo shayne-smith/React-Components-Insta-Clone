6a3a9c9325e4317ef5b6892b63f88495141941d2
Shayne Smith <shayne.m.smith@vanderbilt.edu>
2020-04-15 17:19:25 -0500

    Set up state for DummyData in PostPage.js

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,19 +1,20 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import React, { useState } from "react";
 import Post from "./Post";
 import "./Posts.css";
 // import data 
 import DummyData from "../../dummy-data";
 
-const PostsPage = () => {
+const PostsPage = (props) => {
   // set up state for your data
+  const [postData, setPostData] = useState(DummyData)
 
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
       {
-        DummyData.map( (post, index) => {
+        postData.map( (post, index) => {
           return <Post key={index} username={post.username} likes ={post.likes} thumbnailUrl={post.thumbnailUrl} imageUrl={post.imageUrl} comments={post.comments}/>
         })
       }
